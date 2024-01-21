4abf28bd779e740459970284d508f2acfea92824
MudiLoodi <wadi.38@hotmail.com>
2024-01-21 21:10:35 +0100

    Added Frame widgets to text area and toolbar This allows us to use 'pack()' and 'grid()' without conflicts.

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ from tkinter import ttk
 class HelloWorldApp:
     def __init__(self, root):
         self.root = root
-        self.setup_ui()
+        self.setup_tool_bar()
+        self.setup_text_area()
+        
+    def setup_tool_bar(self):
+        tool_bar_container = Frame(self.root, background="red")
+        tool_bar_container.pack()
+        open_button = Button(tool_bar_container, text="Open")
+        open_button.grid()
+        save_button = Button(tool_bar_container, text="Save")
+        save_button.grid()
 
-    def setup_ui(self):
-        text = Text(self.root)
+    def setup_text_area(self):
+        text_area_container = Frame(self.root)
         # Expand and fill to the full width and height of parent window
+        text_area_container.pack(fill="both", expand=True)
+        
+        text = Text(text_area_container, font="Helvetica 12")
         text.pack(pady=45, padx=45, fill="both", expand=True)
-
+        
 
 if __name__ == "__main__":
     root = Tk()
