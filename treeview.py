4680ef2534255cc5a9d3ed817bb5ed99f8523f85
MudiLoodi <wadi.38@hotmail.com>
2024-01-24 21:15:15 +0100

    Display files in current directory in tree view.

--- treeview.py
+++ treeview.py
@@ -1,20 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
-from tkinter.messagebox import showinfo
-
+import os
 
 class DirectoryTreeView(ttk.Treeview):
     def __init__(self, root):
         super().__init__(root)
-        self.heading('#0', text='Folder name goes here', anchor=tk.W)
-        self.insert('', tk.END, text='Administration', iid=0, open=False)
-        self.insert('', tk.END, text='Logistics', iid=1, open=False)
-        self.insert('', tk.END, text='Sales', iid=2, open=False)
-        self.insert('', tk.END, text='Finance', iid=3, open=False)
-        self.insert('', tk.END, text='IT', iid=4, open=False)
+        current_folder = os.path.basename(os.getcwd())
+        self.heading('#0', text=f"{current_folder}", anchor=tk.W)
 
-        # Adding children of the first node
-        self.insert('', tk.END, text='John Doe', iid=5, open=False)
-        self.insert('', tk.END, text='Jane Doe', iid=6, open=False)
-    
+        files = [f for f in os.listdir() if os.path.isfile(f)]
+        for i in range(0, len(files)):
+            self.insert("", END, text=f"{files[i]}", iid=i, open=False)
